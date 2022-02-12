2599822d0520ea55e9a6517f7c4807fc85cec982
Matt Heffernan <slithymatt@hotmail.com>
2022-02-12 00:00:53 -0500

    finished slides

--- admin.php
+++ admin.php
@@ -2,16 +2,37 @@
 <head>
    <meta charset="utf-8">
    <title>Movopoly Admin Panel</title>
+   <?php
+      $servername = "localhost";
+      $username = "movopoly";
+      $password = "m0v0p0ly";
+      $dbname = "movopoly";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      if (array_key_exists("game",$_REQUEST)) {
+         $game = $_REQUEST["game"];
+
+
+      }
+   ?>
 </head>
 <body>
-<h1>Movopoly Admin Panel</h1>
-<h3>If you don't know what you're doing please leave!</h3>
-&nbsp;<br>
-<b>Delete Games in Progress:</b>
-<div id="gametable">
-</div>
-<b>Delete Pending Games:</b>
-<div id="gametable">
-</div>
+   <h1>Movopoly Admin Panel</h1>
+   <h3>If you don't know what you're doing please leave!</h3>
+   &nbsp;<br>
+   <?php
+      $sql = "SELECT name FROM games WHERE started IS NULL";
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+         echo "<b>Delete Games:</b>";
+
+      }
+   ?>
 </body>
 </html>
